f58a833701d4c8d7f996f39fe11bb0bbab2b8bf8
ByKleitonLima <k.oliveira2002@outlook.com>
2025-02-17 23:22:51 -0300

    Primiero commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Animação ao entrar
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+        duration: 1000, // duração da animação em milissegundos
+        once: true, // anima uma vez quando o elemento entra na tela
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
