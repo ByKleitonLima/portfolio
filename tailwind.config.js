209672ee55cb0e7fc6b7acc3c03a7ead9715d995
ByKleitonLima <k.oliveira2002@outlook.com>
2025-02-21 00:13:17 -0300

    atualizações

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ["./**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'xs': '375px',  // Breakpoint para telas extra pequenas (ex: smartphones)
+                'sm': '640px',  // Breakpoint para telas pequenas (ex: tablets)
+                'md': '768px',  // Alterando o breakpoint médio
+                'lg': '1024px', // Alterando o breakpoint grande
+                'xl': '1536px', // Alterando o breakpoint extra grande
+            }
+        },
     },
         plugins: [],
     };
