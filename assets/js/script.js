9caa341c27259a53095dfdb2105b937e04616256
ByKleitonLima <k.oliveira2002@outlook.com>
2025-04-20 13:57:06 -0300

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Acesso a dispositivos moveis
-
+/*
 function isMobile() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
@@ -8,9 +8,9 @@ if (!isMobile()) {
     document.body.innerHTML = `
         <div class="flex items-center justify-center min-h-screen bg-[#3233232]">
         <div class="text-center p-4">
-            <h1 class="text-2xl font-bold text-[#AAAAAA] mb-4">Acesso Restrito</h1>
-            <p class="text-[#AAAAAA]">Este site está disponível apenas para dispositivos móveis.</p>
+            <h1 class="text-2xl font-bold text-[#AAAAAA] mb-4"> Acesse negado </h1>
+            <p class="text-[#AAAAAA]">Só é possível acessar pelo dispositivo móvel.</p>
         </div>
         </div>`;
 }
-
+*/
